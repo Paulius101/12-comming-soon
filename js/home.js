6930ce896931283297949ceb120888aba56ed38c
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-06-29 14:00:25 +0300

    toast

--- js/home.js
+++ js/home.js
@@ -2,16 +2,27 @@
 import { Clock } from "./components/clock/Clock.js";
 import { Form } from "./components/form/Form.js";
 import { Progressbar } from "./components/progress-bar/ProgressBar.js";
-// import { socials } from "./components/socials/socials.js";
+import { socials } from "./components/socials/socials.js";
+import { SocialsOOP } from "./components/socials/SocialsOOP.js";
+import { Toast } from "./components/toast/Toast.js";
 import { clockData } from "./data/clockData.js";
 import { progressBarData } from "./data/progressBarData.js";
 import { socialsData } from "./data/socialsData.js";
-import { SocialsOOP } from "./components/socials/SocialsOOP.js";
 
 // EXECUTION
+const toast = new Toast;
+toast.success('Tau pavyko!');
+toast.info('Pranesame apie si bei ta!');
+toast.warning('Tu cia ziurek..!');
+toast.error('Yra klaida!')
+// toast.error('Yra klaida!', 'Nauja antraste');
+
+
+
+
 new Clock('#clock_1', clockData);
 // socials('footer .socials', socialsData);
 new Progressbar('.left-column', progressBarData);
-new SocialsOOP('.socials', socialsData);
+new SocialsOOP('footer .socials', socialsData);
 new Form('.hero form');
 new Form('main form');
